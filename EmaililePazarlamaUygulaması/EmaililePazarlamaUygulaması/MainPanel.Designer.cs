0cc28b565d863de960f90642713d1b2a9c02cd6f
tuncemran <yte202@exeter.ac.uk>
2020-09-14 22:54:44 +0300

    Utilities Class first draft

---
+++ EmaililePazarlamaUygulaması/EmaililePazarlamaUygulaması/MainPanel.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace EmaililePazarlamaUygulaması
+{
+    partial class MainPanel
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(35, 63);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(560, 540);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(708, 63);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(560, 540);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView3
+            // 
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(1350, 249);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(489, 618);
+            this.listView3.TabIndex = 2;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(35, 691);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1233, 176);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "YENİ ETKİLEŞİM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(31, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "EMAIL LİSTELERİ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1350, 63);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(489, 135);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(1346, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "GEÇMİŞ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(1346, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "SERVİS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(704, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "EMAIL ŞABLONLARI";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // MainPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1876, 920);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.MaximumSize = new System.Drawing.Size(1898, 976);
+            this.MinimumSize = new System.Drawing.Size(1898, 976);
+            this.Name = "MainPanel";
+            this.ShowIcon = false;
+            this.Text = "EMAIL PAZARLAMA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EmaililePazarlamaUygulaması/EmaililePazarlamaUygulaması/Utilties.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace EmaililePazarlamaUygulaması
+{
+    public static class Utilties
+    {
+        public static string GetFilePath(string filePath)
+        {
+            char[] delims = new[] { '\r', '\n', '\\' };
+            string fileName = filePath;
+            string currentDirectory = Environment.CurrentDirectory;
+            string changeDirectory = "";
+            string[] directorySplit = currentDirectory.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < directorySplit.Length - 2; i++)
+            {
+                changeDirectory = changeDirectory + directorySplit[i] + '\\';
+            }
+
+            ;
+            string path = System.IO.Path.Combine(changeDirectory, @"Data\", fileName);
+            return path;
+        }
+
+        //List directory and return string array
+        public static string[] ListDirectory(string path,bool files)
+        {
+            string[] filePaths;
+            string directoryPath = GetFilePath(path);
+            if (files)
+            {
+                filePaths = Directory.GetFiles(directoryPath);
+            }
+            else
+            {
+                filePaths = Directory.GetDirectories(directoryPath);
+            }
+            
+            return filePaths;
+        }
+
+        //Check if file exists or create file and return file
+        public static void CreateFile(string filePath)
+        {
+            string directoryPath = GetFilePath(filePath);
+            if (!File.Exists(directoryPath))
+            {
+                File.Create(directoryPath);
+            }
+        }
+        //File Write operation
+        public static void WriteFile(string filePath, string str)
+        {
+            string directoryPath = GetFilePath(filePath);
+            File.AppendAllText(directoryPath, str + Environment.NewLine);
+        }
+
+        //File Read operation
+        public static string[] ReadFile(string filePath)
+        {
+            string directoryPath = GetFilePath(filePath);
+            string[] lines = File.ReadAllLines(directoryPath);
+            return lines;
+        }
+
+        //HTTP Request
+        public static void SendEmail(string addresses, string subject, string code)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://makasapp.com/v1/email/offers");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = new JavaScriptSerializer().Serialize(new
+                {
+                    address = addresses,
+                    subject = subject,
+                    code = code
+                });
+
+                streamWriter.Write(json);
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+        }
+    }
+}
